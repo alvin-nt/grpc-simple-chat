5940f1a241f2e2a87e9e979e7d276679784863e6
Alvin Natawiguna <alvin.nt.gg@gmail.com>
2015-09-29 11:52:39 +0700

    Refactored

--- generate-java-proto.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# assumes that you have protoc installed
-protoc --plugin=protoc-gen-grpc-java=./protoc-gen-grpc-java --java_out=client/java --grpc-java_out=client/java ChatService.proto
